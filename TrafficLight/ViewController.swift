fac1244009b7d6e6a9659ad11d41bed05b67a97a
Dmitrandir <dim9107@mail.ru>
2022-07-31 21:19:59 +0300

    Traffic Light completed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = 65
+        yellowLight.layer.cornerRadius = 65
+        greenLight.layer.cornerRadius = 65
+        
+        button.layer.cornerRadius = 15
+        
     }
-
-
+    
+    @IBAction func buttonPressed() {
+        
+        if redLight.alpha != 1 && yellowLight.alpha != 1 {
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+            button.setTitle("NEXT", for: .normal)
+        } else if yellowLight.alpha != 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        } else if greenLight.alpha != 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        }
+    }
 }
 
